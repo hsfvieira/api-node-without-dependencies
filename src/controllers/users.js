50cbb88d516768e6c0cde99810cd1885b68bbcb6
Henrique Vieira <hsfvieira@gmail.com>
2020-04-16 14:53:49 -0300

    Fixed import of user model

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,27 +1,18 @@
-import { 
-    count, 
-    countFromFirstName,
-    selectAll,
-    selectFromFirstName, 
-    selectFromID, 
-    insert, 
-    updateOne, 
-    deleteOne 
-} from '../models/users.js'
+import * as userModel from '../models/users.js'
 
 export async function index(req, res) {
     const { page } = !req.query.page ? { page: 1 } : req.query
 
     if(req.query.first_name) {
         const { first_name } = req.query
-        const totalLines = await countFromFirstName(first_name)
-        const data = await selectFromFirstName(first_name, (page - 1) * 10)
+        const totalLines = await userModel.countFromFirstName(first_name)
+        const data = await userModel.selectFromFirstName(first_name, (page - 1) * 10)
 
         res.setHeader('X-Total-Count', totalLines)
         res.json(data)
     } else {
-        const totalLines = await count()
-        const data = await selectAll((page - 1) * 10)
+        const totalLines = await userModel.count()
+        const data = await userModel.selectAll((page - 1) * 10)
         res.setHeader('X-Total-Count', totalLines)
         res.json(data)
     }
@@ -29,7 +20,7 @@ export async function index(req, res) {
 
 export async function view(req, res) {
     const { id } = req.params
-    const data = await selectFromID(id)
+    const data = await userModel.selectFromID(id)
 
     if(data) {
         res.json(data)
@@ -40,7 +31,7 @@ export async function view(req, res) {
 
 export async function create(req, res) {
     const data = req.body
-    await insert(data)
+    await userModel.insert(data)
 
     res.statusCode = 201
     res.end()
@@ -50,7 +41,7 @@ export async function update(req, res) {
     const { id } = req.params
     const newObj = req.body
 
-    const result = await updateOne(id, newObj)
+    const result = await userModel.updateOne(id, newObj)
 
     if(!result) {
         res.statusCode = 404
@@ -64,13 +55,13 @@ export async function update(req, res) {
 export async function del(req, res) {
     const { id } = req.params
     
-    const result = await deleteOne(id)
+    const result = await userModel.deleteOne(id)
 
     if(!result) {
         res.statusCode = 404
     } else {
         res.statusCode = 204
     }
-    
+
     res.end()
 }
