376428627d03c6431cbc6461a00156fa1e390958
Henrique Vieira <hsfvieira@gmail.com>
2020-04-24 19:30:20 -0300

    added html

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import * as app from './http/index.js'
 import * as user from './controllers/users.js'
+import fs from 'fs'
 
 app.get('/users', user.index)
+
+app.get('/users_table', (req, res) => {
+    fs.readFile('./src/views/users_table.html', (err, data) => {
+        if(err) console.log(err)
+        res.html(data)  
+    })
+})
 
 app.get('/users/:id', user.view)
 
